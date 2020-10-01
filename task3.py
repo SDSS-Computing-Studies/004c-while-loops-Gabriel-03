cfa7b972fc8187b9777268c41708f3b7fd5abfd7
Gabriel-03 <71287702+Gabriel-03@users.noreply.github.com>
2020-10-01 11:35:57 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -18,10 +18,13 @@ That is not an even integer
 Enter number:4
 That is an even integer
 """
-n = 1
-while n % 2 != 0:
-    n = float(input("Enter a number\n"))
-    if n % 2 != 0:
-        print("That is not an even integer.")
-    else:
-        print("That is an even integer.")\
+a = input("Enter a number: ").strip()
+a = float(a)
+
+while (a % 2 != 0):
+    print("That is not an even integer")
+    a = input("Enter a number: ").strip()
+
+    break
+
+print("That is an even integer")
